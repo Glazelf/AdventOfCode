9c4e5cc53313d0da66a5b1b233e8063396df85ba
Glazelf <gideon.steenbergen@gmail.com>
2022-12-04 11:10:07 +0100

    d4 minor refactor

--- 2022/4/index.js
+++ 2022/4/index.js
@@ -4,16 +4,14 @@ eval(fs.readFileSync("./input.js", "utf8"));
 let part1score = 0;
 let part2score = 0;
 for (pair of input) {
-    let pair1low = parseInt(pair[0].split("-")[0]);
-    let pair1high = parseInt(pair[0].split("-")[1]);
-    let pair2low = parseInt(pair[1].split("-")[0]);
-    let pair2high = parseInt(pair[1].split("-")[1]);
+    let pair1 = [parseInt(pair[0].split("-")[0]), parseInt(pair[0].split("-")[1])];
+    let pair2 = [parseInt(pair[1].split("-")[0]), parseInt(pair[1].split("-")[1])];
     let range1 = [];
     let range2 = [];
-    for (let i = pair1low; i <= pair1high; i++) {
+    for (let i = pair1[0]; i <= pair1[1]; i++) {
         range1.push(i);
     };
-    for (let i = pair2low; i <= pair2high; i++) {
+    for (let i = pair2[0]; i <= pair2[1]; i++) {
         range2.push(i);
     };
     if (range1.every(i => range2.includes(i)) || range2.every(i => range1.includes(i))) part1score += 1;
